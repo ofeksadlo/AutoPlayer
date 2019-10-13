a2c8f9ccef8f8dacf9171629ea7a8258be01e98e
ofeksadlo <55595248+ofeksadlo@users.noreply.github.com>
2019-10-13 19:21:41 +0100

    Add files via upload

---
+++ Settings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoPlayer
+{
+    class Settings
+    {
+        public static void UpdateSettings(int lastPlayed, string Path)
+        {
+            AutoPlayer.Properties.Settings.Default.LastPlayed = lastPlayed;
+            AutoPlayer.Properties.Settings.Default.Path = Path;
+            AutoPlayer.Properties.Settings.Default.Saved = true;
+            AutoPlayer.Properties.Settings.Default.Save();
+        }
+        public static void Save()
+        {
+            AutoPlayer.Properties.Settings.Default.Saved = true;
+            AutoPlayer.Properties.Settings.Default.Save();
+        }
+        public static void dontSave()
+        {
+            AutoPlayer.Properties.Settings.Default.Saved = false;
+            AutoPlayer.Properties.Settings.Default.Save();
+        }
+    }
+}
