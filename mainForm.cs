95b2564416c887961fcfde02d956ca28d5c05310
ofeksadlo <55595248+ofeksadlo@users.noreply.github.com>
2019-10-20 20:45:50 +0100

    Update mainForm.cs

--- mainForm.cs
+++ mainForm.cs
@@ -10,13 +10,15 @@ namespace AutoPlayer
         {
             InitializeComponent();
         }
-        string nextEposidePath;
+        Clip currentClip;// Represents the clip the user is currently viewing.
         private void mainForm_Load(object sender, EventArgs e)
         {
+            axWindowsMediaPlayer.enableContextMenu = false;
+            axWindowsMediaPlayer.CustomContextMenu = contextMenuStrip;
             //uiMode means - User Interface Mode. The program will create a new file called uiMode in wich you can change the value 1 which means full User Interface.
             if (File.Exists(Environment.CurrentDirectory + "\\uiMode.ini"))//Into 0 wich mean no User Interface great for bing watching something.
             {
-                if(File.ReadAllText(Environment.CurrentDirectory + "\\uimode.ini") == "0")
+                if (File.ReadAllText(Environment.CurrentDirectory + "\\uimode.ini") == "0")
                 {
                     axWindowsMediaPlayer.uiMode = "none";
                 }
@@ -28,84 +30,123 @@ namespace AutoPlayer
             else
             {
                 File.WriteAllText(Environment.CurrentDirectory + "\\uiMode.ini", "1");//The starting value is 1 but anything except 0 will do the same.
-            }// The only situation the Saved setting will be true and LastPlayed will be 0. Is when an eposide has been changed automatically.
-            if (AutoPlayer.Properties.Settings.Default.Saved == true && AutoPlayer.Properties.Settings.Default.LastPlayed == 0)
-            {//In this case the client is watching a series. And we want the new eposide to start immediately.
-                axWindowsMediaPlayer.Ctlcontrols.currentPosition = AutoPlayer.Properties.Settings.Default.LastPlayed;
-                axWindowsMediaPlayer.URL = AutoPlayer.Properties.Settings.Default.Path;
-                this.Text = axWindowsMediaPlayer.URL;
             }
-            else if(AutoPlayer.Properties.Settings.Default.Saved == true)
-            {//In this case the client is not watching a series. But closed the video while saving the exit point.
-                if (MessageBox.Show("?אתה רוצה להמשיך מאותה נקודה", AutoPlayer.Properties.Settings.Default.Path, MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
-                {
-                    axWindowsMediaPlayer.Ctlcontrols.currentPosition = AutoPlayer.Properties.Settings.Default.LastPlayed;
-                    axWindowsMediaPlayer.URL = AutoPlayer.Properties.Settings.Default.Path;
-                    this.Text = axWindowsMediaPlayer.URL;
-                }
-                else
-                {
-                    if(openFileDialog.ShowDialog()==DialogResult.OK)
-                    {
-                        axWindowsMediaPlayer.URL = openFileDialog.FileName;
-                    }
-                    else
-                    {
-                        Application.Exit();
-                    }
-                }
-            }//This is the case the client either never opened the program before. Or didn't saved the time position.
+            if(AutoPlayer.Properties.Settings.Default.LastPlayedPath !="") // Checks if the player has ever been used for playing a clip
+            {//If it did it will play the last position of the clip.
+                currentClip = Clip.getClipByPath(AutoPlayer.Properties.Settings.Default.LastPlayedPath);
+                axWindowsMediaPlayer.Ctlcontrols.currentPosition = currentClip.Time;
+                axWindowsMediaPlayer.URL = currentClip.Path;
+                this.Text = currentClip.Path;
+            }
             else
-            {
-                if (openFileDialog.ShowDialog() == DialogResult.OK)
-                {
-                    axWindowsMediaPlayer.URL = openFileDialog.FileName;
-                }
-                else
-                {
-                    Application.Exit();
-                }
+            {//If the player never been used it will show the open media dialog.
+                chooseNewClip();
             }
-            nextEposidePath = Series.getNextEposidePath(openFileDialog.FileName);
         }
-        bool clipFinished = false;// Determines if the video is finished.
-        private void mainForm_FormClosing(object sender, FormClosingEventArgs e)// In a case of the client closing the player we will ask the client weather he want to save
-        {//                                                                        the time position.
-            if (axWindowsMediaPlayer.URL != "" && clipFinished == false && nextEposidePath == "")
-            {
-                if (MessageBox.Show("לשמור לך תזמן צפייה אחי?", "", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
-                {
-                    Settings.UpdateSettings((int)axWindowsMediaPlayer.Ctlcontrols.currentPosition, axWindowsMediaPlayer.URL);
-                    
+
+        private void mainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {// In a case of the client closing the player. We want to save the last position of the clip so we use this event function.
+                if (axWindowsMediaPlayer.URL != "")
+            {// We check the url is not null because in a case of first running the program. And closing the open media dialog it will cause a null exception.
+                if (Clip.getClipByPath(axWindowsMediaPlayer.URL)== null)
+                    {//If the clip doesn't exists in our database at this line of code. It means the client changed eposides
+                    //until he got to a clip which doesn't exists in our database. The reason is that changing eposides doesn't add clips to the database.
+                        Clip.addClipToDatabase(axWindowsMediaPlayer.URL);
+                    }
+                    Clip.updateClipTimeFromDatabase(axWindowsMediaPlayer.URL, (int)axWindowsMediaPlayer.Ctlcontrols.currentPosition);
+                    AutoPlayer.Properties.Settings.Default.LastPlayedPath = currentClip.Path;
+                    AutoPlayer.Properties.Settings.Default.Save();
                 }
-                else
-                {
-                    Settings.dontSave();
-                }
-            }
-            else if(nextEposidePath != "")
-            {
-                Series.nextEposide(nextEposidePath);
-            }
-            
         }
 
         private void axWindowsMediaPlayer_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
         {
+            if (axWindowsMediaPlayer.playState == WMPLib.WMPPlayState.wmppsMediaEnded)
+            {
+                currentClip = Clip.getNextClip(currentClip);
+                if (currentClip != null)
+                {
+                    this.Text = currentClip.Path;
+                    this.BeginInvoke(new Action(() => {
+                        this.axWindowsMediaPlayer.URL = currentClip.Path;
+                    }));
+                }
+                else
+                {
+                    chooseNewClip();
+                }
+            }
+            if(axWindowsMediaPlayer.playState == WMPLib.WMPPlayState.wmppsPlaying)
+            {
+                this.axWindowsMediaPlayer.fullScreen = true;
+            }
+        }
 
-            if (axWindowsMediaPlayer.playState == WMPLib.WMPPlayState.wmppsMediaEnded && nextEposidePath != "")
+        private void יציאהToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //forcedCloseing = true;
+            Application.Exit();
+        }
+        private void chooseNewClip()
+        {
+            if(currentClip != null)
             {
-                clipFinished = true;
-                axWindowsMediaPlayer.fullScreen = false;
-                Application.Exit();
+                Clip.updateClipTimeFromDatabase(axWindowsMediaPlayer.URL, (int)axWindowsMediaPlayer.Ctlcontrols.currentPosition);
             }
-            else if (axWindowsMediaPlayer.playState == WMPLib.WMPPlayState.wmppsMediaEnded && nextEposidePath == "")
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
-                clipFinished = true;
-                axWindowsMediaPlayer.fullScreen = false;
-                Settings.dontSave();
-                Application.Exit();
+                currentClip = Clip.getClipByPath(openFileDialog.FileName);
+                if (currentClip == null)
+                {
+                    Clip.addClipToDatabase(openFileDialog.FileName);
+                    currentClip = new Clip(openFileDialog.FileName, 0);
+                }
+                axWindowsMediaPlayer.URL = openFileDialog.FileName;
+                axWindowsMediaPlayer.Ctlcontrols.currentPosition = currentClip.Time;
+                this.Text = axWindowsMediaPlayer.URL;
             }
+            else
+            {
+                currentClip = new Clip(axWindowsMediaPlayer.URL, (int)axWindowsMediaPlayer.Ctlcontrols.currentPosition);
+            }
+        }
+
+        private void חדשToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            chooseNewClip();
+        }
+
+        private void פרק_הבאToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Clip.updateClipTimeFromDatabase(currentClip.Path, (int)axWindowsMediaPlayer.Ctlcontrols.currentPosition);
+            currentClip = Clip.getNextClip(currentClip);
+            if(currentClip != null)
+            {
+                this.Text = currentClip.Path;
+                axWindowsMediaPlayer.URL = currentClip.Path;
+                axWindowsMediaPlayer.Ctlcontrols.currentPosition = currentClip.Time;
+            }
+            else
+            {
+                chooseNewClip();
+            }
+        }
+
+        private void פרקקודםToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Clip.updateClipTimeFromDatabase(currentClip.Path, (int)axWindowsMediaPlayer.Ctlcontrols.currentPosition);
+            currentClip = Clip.getPreviousClip(currentClip);
+            if (currentClip != null)
+            {
+                this.Text = currentClip.Path;
+                axWindowsMediaPlayer.URL = currentClip.Path;
+                axWindowsMediaPlayer.Ctlcontrols.currentPosition = currentClip.Time;
+            }
+            else
+            {
+                chooseNewClip();
+            }
+           
         }
     }
 }
